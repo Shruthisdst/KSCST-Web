84f4b323c9f226be3243f6758cf15ea61f273400
Shruthisdst <shruthitr.nayak@gmail.com>
2016-03-09 18:12:24 +0530

    Commit No. 10 - patent awareness page added

--- application/views/flat/4-Programmes/5-Patent_Information_Centre/index.php
+++ application/views/flat/4-Programmes/5-Patent_Information_Centre/index.php
@@ -5,4 +5,52 @@
 <p>Intellectual property is a "Product of Mind". It is similar to the property consisting of movable and immovable property which can be used by the owner along and not lawfully by others without owners permission. World Intellectual Property Organization (WIPO) provides that "Intellectual Property" includes rights related to works in literary and artistic fields, performance of artists, broadcasts (in the form of CD, DVD, tape recording etc), innovations in science and technology, industrial designs, trade marks, service marks, etc.</p>
 <p>Inventions in all fields of human endeavour and protection against unfair competition and all other rights resulting from intellectual activity in the industrial, scientific or artistic fields.</p>
 <h3>Forms of protection</h3>
-<p><strong>1. Patents :</strong> Patent is an exclusive right granted by a country to the owner of an innovation to make, use, manufacture and market the invention, provided the innovation satisfies certain criteria - like having novelty, inventive step and industrial application (<a href="patent_faqs" target="_blank">please refer FAQs about Patents</a>).</p>
+<ol>
+	<li class="gap-above-small"><strong>Patents :</strong> Patent is an exclusive right granted by a country to the owner of an innovation to make, use, manufacture and market the invention, provided the innovation satisfies certain criteria - like having novelty, inventive step and industrial application (<a href="patent_faqs" target="_blank">please refer FAQs about Patents</a>).</li>
+	<li class="gap-above-small"><strong>Copyrights :</strong> Copyright is a right granted to the owner for creating an original literary or dramatic or musical or artistic work. Cinematographic films including sound track and video films and recordings on discs, tapes etc are also covered by copyrights. Computer programs and software are covered under literary works and are protected in India under copyrights.</li>
+	<li class="gap-above-small"><strong>Trademarks :</strong> Trademark is a distinctive sign, which identifies certain goods or services as those produced or provided by a specific person or enterprise. Trademarks may be one or combination of words, letters, and numerals and may also consist of drawings, symbols, two / three dimensional signs or colours, used as distinguishing feature.</li>
+	<li class="gap-above-small"><strong>Industrial design :</strong> Industrial Design means the features of shape, configuration, pattern, ornament or composition of lines or colours applied to any article by any industrial or manual process or means and which are judged solely by the perception of the eye.</li>
+	<li class="gap-above-small"><strong>Geographical indications :</strong> Geographical Indications (GI) means an indication which identifies certain agricultural or natural or manufactured goods as originating or manufactured in the territory of a country, or a region or locality in that territory, where a given quality, reputation or other characteristics of such goods is essentially attributable to its geographical origin.</li>
+	<li class="gap-above-small"><strong>Layout design of integrated circuits :</strong> Layout Design of Integrated Circuit includes a layout of transistors and other circuitry elements and includes lead wires connecting such elements and expressed in any manner in a semiconductor integrated circuit (IC), designed to perform an electronic circuitry function.</li>
+	<li class="gap-above-small"><strong>Trade secrets :</strong> Trade Secret, also referred as undisclosed information, includes formula, pattern, compilation, programme, device, method, technique or process. It is perhaps the most important form of protection for industries and R&amp;D institutions.</li>
+	<li class="gap-above-small"><strong>Plant variety protection :</strong> Plant Variety Protection and Farmer's Rights (PVPFR) is a system for protection of plant varieties, the rights of farmers and plant breeders and to encourage the development of new varieties of plants. Criteria for protection a new plant variety are distinctness, uniformity, stability, novelty and appropriate denomination (generic designation).</li>
+</ol>
+<h3>Who are benefited?</h3>
+<p>The owner of such intellectual property can make use of the property for commercial exploitation and also can prohibit infringement by others. This plays a key role in gaining advantageous position in these days of competition for technological development and consequent economic growth of the country. This has become more pronounced in present day global economy with so many international agreements and obligations in the field of IPR in the World Trade Organisation (WTO) regime.</p>
+<h3>OBJECTIVES OF PATENT CELL</h3>
+<ul>
+	<li class="gap-above-small">To create awareness about IPRs, especially patents, in the state and neighboring regions of this centre.</li>
+	<li class="gap-above-small">To enable patent searches for universities, industry, government departments and R&amp;D institutions in the State.</li>
+	<li class="gap-above-small">To guide inventors in respect of patenting their inventions.</li>
+</ul>
+<h3>PATENT AWARENESS PROGRAMMES AT REGIONAL CENTRES</h3>
+<p>As a first step towards achieving these objectives, KSCST, with the support of <a href="http://www.tifac.org.in/" target="_blank">TIFAC</a>, Department of Science and Technology, New Delhi intends to conduct "Patent Awareness Programmes" at some regional centres to cover the entire state. The main objective of these programmes is to make technocrats, academicians and industry aware of patents and patenting system, other forms of IPRs like copyrights, trademarks etc, relevant procedures and implications of such act of holding IPRs. It is intended that the participants for these programmes are to include people from industry, students and faculty from universities, medical, engineering and agricultural colleges, polytechnics, research institutions, other innovators in and around these regional centres and neighbouring districts.</p>
+<h3>PATENT AWARENESS PROGRAMME AT REGIONAL CENTRES</h3>
+<ul>
+	<li class="gap-above-small"><a href="patent_awareness#Patent_Awareness_Programme_at_Gulbarga" target="_blank">Patent Awareness Programme at Gulbarga</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Patent_Awareness_Programme_at_Shimoga" target="_blank">Patent Awareness Programme at Shimoga</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Patent_Awareness_Programme_at_Belgaum" target="_blank">Patent Awareness Programme at Belgaum</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Patent_Awareness_Programme_at_Mangalore" target="_blank">Patent Awareness Programme at Mangalore</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Patent_Awareness_Programme_at_Hassan" target="_blank">Patent Awareness Programme at Hassan</a></li>
+</ul>
+<h3><a href="patent_awareness#IPR_Awareness_Programmes" target="_blank">IPR AWARENESS PROGRAMMES ORGANISED AT LOCAL ENGINEERING COLLEGES / INSTITUTES</a></h3>
+<ul>
+	<li class="gap-above-small"><a href="patent_awareness#SIET_Tumkur" target="_blank">Shridevi Institute of Engineering and Technology, Tumkur</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#BLDEA_Bijapur" target="_blank">BLDEA's College of Engineering and Technology, Bijapur</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#IISc_Bengaluru" target="_blank">Indian Institute of Science, Bangalore</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Ambedkar_Institute_of_Technology" target="_blank">Dr. Ambedkar Institute of Technology, Bangalore</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Reva_Institute_of_Technology" target="_blank">Reva Institute of Technology and Management, Bangalore</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#Manipal_Institute_of_Management" target="_blank">National Workshop at Manipal Institute of Management</a></li>
+</ul>
+<h3>IPR WORKSHOPS ORGANISED JOINTLY WITH KARNATATAKA COUNCIL FOR TECHNOLOGICAL UPGRADATION (KCTU)</h3>
+<ul>
+	<li class="gap-above-small"><a href="patent_awareness#Patent_Workshop_at_Hubli" target="_blank">Patent Workshop at Hubli</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#IPR_workshop_at_shimoga" target="_blank">IPR Workshop at District Industries Centre - Shimoga</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#IPR_workshop_at_Mysore" target="_blank">IPR Workshop at District Industries Centre - Mysore</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#IPR_workshop_at_Belgaum" target="_blank">IPR Workshop at District Industries Centre - Belgaum</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#" target="_blank">IPR Workshop at District Industries Centre - Mangalore</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#" target="_blank">IPR WORKSHOP - Gulbarga</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#" target="_blank">IPR Workshop - Kolar</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#" target="_blank">IPR Workshop - Davangere</a></li>
+	<li class="gap-above-small"><a href="patent_awareness#" target="_blank">IPR Workshop - Raichur</a></li>
+</ul>
